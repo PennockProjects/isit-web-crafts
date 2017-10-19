ea2df78f2ec60c9a5d2f56517239d75926adac69
John in vbox on xoticpc <john.pennock@bellevuecollege.edu>
2017-10-18 17:19:52 -0700

    work in class on the web crafts react starter

---
+++ source/ReactHome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import HomeButtons from './HomeButtons'
+
+export default class ReactBasics extends React.Component {
+ render() {
+     return <div>
+         <h1>An H1 element in a React Component</h1>
+         <HomeButtons/>
+     </div>
+ }
+}
